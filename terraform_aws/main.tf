9b76333821cc48fd8293690079c146ed0a2f90ba
MEAKSOY <mesataksoy@gmail.com>
2022-07-23 08:12:04 +0000

    Tf file is added.

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -1,23 +1,46 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.22.0"
+      version = "4.23.0"
     }
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+locals {
+  mytag = "esat-local-name"
+}
+
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-0ed9277fb7eb570c9"
-  instance_type = "t2.micro"
-  key_name      = "firstkey" # write your pem file without .pem extension>
+  ami           = var.ec2_ami
+  instance_type = var.ec2_type
+  key_name      = "firstkey"
   tags = {
-    "Name" = "tf-ec2"
+    "Name" = "${local.mytag}-come from locals"
   }
+}
+
+resource "aws_s3_bucket" "tf-s3" {
+  bucket = "${var.s3_bucket_name}-${count.index}"
+  count = var.num_of_buckets
+}
+
+
+output "tf_example_public_ip" {
+  value = aws_instance.tf-ec2.public_ip
+}
+
+# output "tf_example_s3_meta" {
+#   value = aws_s3_bucket.tf-s3[*]
+# }
+
+output "tf_example_private_ip" {
+  value = aws_instance.tf-ec2.private_ip
 }
 
 resource "aws_s3_bucket" "tf-s3" {
