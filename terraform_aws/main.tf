f5ffb26319ce191c8e152f4712e8907d2d0b9e82
MEAKSOY <mesataksoy@gmail.com>
2022-07-23 09:27:39 +0000

    File is modified.

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -16,8 +16,17 @@ locals {
   mytag = "esat-local-name"
 }
 
+data "aws_ami" "tf_ami" {
+  most_recent = true
+  owners       = ["self"]
+  filter {
+    name  = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "tf-ec2" {
-  ami           = var.ec2_ami
+  ami           = data.aws_ami.tf_ami.id
   instance_type = var.ec2_type
   key_name      = "firstkey"
   tags = {
@@ -26,13 +35,24 @@ resource "aws_instance" "tf-ec2" {
 }
 
 resource "aws_s3_bucket" "tf-s3" {
-  bucket = "${var.s3_bucket_name}-${count.index}"
-  count = var.num_of_buckets
+  # bucket = "${var.s3_bucket_name}-${count.index}"
+  # count = var.num_of_buckets
+  # count = var.num_of_buckets != 0 ? var.num_of_buckets : 3
+  for_each = toset(var.users)
+  bucket   = "esat-tf-s3-bucket-${each.value}-ams"
 }
 
+resource "aws_iam_user" "new_users" {
+  for_each = toset(var.users)
+  name     = each.value
+}
 
 output "tf_example_public_ip" {
   value = aws_instance.tf-ec2.public_ip
+}
+
+output "uppercase_users" {
+  value = [for user in var.users : upper(user) if length(user) > 6]
 }
 
 # output "tf_example_s3_meta" {
@@ -43,6 +63,6 @@ output "tf_example_private_ip" {
   value = aws_instance.tf-ec2.private_ip
 }
 
-resource "aws_s3_bucket" "tf-s3" {
+resource "aws_s3_bucket" "tf-s3-1" {
   bucket = "esat-tf-test-bucket-new"
 }
--- terraform_aws/variable.tf
+++ terraform_aws/variable.tf
@@ -6,9 +6,9 @@ variable "ec2_type" {
   default = "t2.micro"
 }
 
-variable "ec2_ami" {
-  default = "ami-0cff7528ff583bf9a"
-}
+# variable "ec2_ami" {
+#   default = "ami-0cff7528ff583bf9a"
+# }
 
 variable "num_of_buckets" {
   default = 2
@@ -16,5 +16,9 @@ variable "num_of_buckets" {
 
 
 variable "s3_bucket_name" {
-#   default = "esat-s3-bucket-variable-new"
+  default = "esat-s3-bucket-variable-new"
+}
+
+variable "users" {
+  default = ["santino", "michael", "fredo"]
 }
